6a5ffc5f6f190483c50631ca4fc91d4f8f873dd7
gurinina <g.giaever@ubc.ca>
2022-12-01 12:35:12 -0800

    exploratory

--- RNA-genomics/subreadMapping.sh
+++ RNA-genomics/subreadMapping.sh
@@ -18,7 +18,7 @@ subread-buildindex -o r64/r64bread.index r64/Saccharomyces_cerevisiae.R64-1-1.dn
 # use as many 'threads' as your machine will allow in order to speed up the read mapping process.
 # note that we're also including the '&>' at the end of each line
 # this takes the information that would've been printed to our terminal, and outputs this in a log file that is saved in /data/course_data
-samtools view -h ERR458493.bam | grep -e "(NH:i:1|^@)" | 
+ 
 # aligning
 subread-align --sortReadsByCoordinates -a r64/Saccharomyces_cerevisiae.R64-1-1.96.gtf.gz -B 3 -T 16 -i r64/r64bread.index -t 0 -r ERR458493.fastq.gz -o ../BAM/ERR458493.bam &> ../BAM/R493.log
 subread-align --sortReadsByCoordinates -a r64/Saccharomyces_cerevisiae.R64-1-1.96.gtf.gz -B 3 -T 16 -i r64/r64bread.index -t 0 -r ERR458494.fastq.gz -o ../BAM/ERR458494.bam &> ../BAM/R494.log
@@ -31,27 +31,38 @@ subread-align --sortReadsByCoordinates -a r64/Saccharomyces_cerevisiae.R64-1-1.9
 featureCounts -a r64/Saccharomyces_cerevisiae.R64-1-1.96.gtf.gz -M -o ../counts.txt ../BAM/*.bam
 # Multi-mapping reads
 
-  # -M                  Multi-mapping reads will also be counted. For a multi-
-  #                     mapping read, all its reported alignments will be 
-  #                     counted. The 'NH' tag in BAM/SAM input is used to detect 
-  #                     multi-mapping reads. If a gene annotations file is used during the map/align stage, and the splice junction is detected as an annotated junction, then 20 is added to its motif value.
-  # multi-mapping reads are not to be confused with
-  # NH:i—A standard SAM tag indicating the number of reported alignments that contains the query in the current record. This tag may be used for downstream tools such as featureCounts.
+  # -M Multi-mapping reads will also be counted. For a multi-
+  #    mapping read, all its reported alignments will be 
+  #    counted. The 'NH' tag in BAM/SAM input is used to detect 
+  #    multi-mapping reads. If a gene annotations file is used during 
+  #    the map/align stage, 
+  #    and the splice junction is detected as an annotated junction, 
+  #    then 20 is added to its motif value.
+  #    
+  # -O Assign reads to all their overlapping meta-features (or 
+  #      features if -f is specified).
+  # -f Perform read counting at feature level (eg. counting 
+  #      reads for exons rather than genes).
+  #      
 
+  
+  #    multi-mapping reads are not to be confused with
+  #    NH:i—A standard SAM tag indicating the number of reported 
+  #    alignments that contains the query in the current record. 
+  #    This tag may be used for downstream tools such as featureCounts.
 
-
--t 'exon' is unnecessary
-# -t <string> Specify feature type(s) in a GTF annotation. If multiple
-                      # types are provided, they should be separated by ',' with
-                      # no space in between. 'exon' by default. Rows in the
-                      # annotation with a matched feature will be extracted and
-                      # used for read mapping.
-# -g gene_id is unnecessary
-# -g <string> Specify attribute type in GTF annotation. 'gene_id' by 
+  #   -t 'exon' is unnecessary
+  #   -t <string> Specify feature type(s) in a GTF annotation. 
+  #    If multiple
+  #    types are provided, they should be separated by ',' with
+  #    no space in between. 'exon' by default. Rows in the
+  #    annotation with a matched feature will be extracted and
+  #    used for read mapping.
+  # -g gene_id is unnecessary
+  # -g <string> Specify attribute type in GTF annotation. 'gene_id' by 
                       # default. Meta-features used for read counting will be 
                       # extracted from annotation using the provided value.
-# -O Assign reads to all their overlapping meta-features (or 
-                      #features if -f is specified)
+  
 # summarize fastqc and subread mapping results in a single summary html using MultiQC
 multiqc -d . ../FASTQC
 
